c52f820bf0df4329f2acaaff14fc8be2efc24128
Viktor Ivannikov <iceivannikov@gmail.com>
2024-03-26 20:12:45 +0300

    6. Правила Checkstyle [#504981 #487793]

--- src/main/java/ru/job4j/checkstyle/Broken.java
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -1,9 +1,13 @@
 package ru.job4j.checkstyle;
 
 public class Broken {
+
     private static final String NEW_VALUE = "";
+
     private int sizeOfEmpty = 10;
+
     private String name;
+
     private String surname;
 
     public Broken() {
