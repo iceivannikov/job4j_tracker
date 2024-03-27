333ebc5b8bb752c0004fe8d905deaaacb24e1a0a
Viktor Ivannikov <iceivannikov@gmail.com>
2024-03-27 19:53:57 +0300

    8. Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,8 +1,14 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+
 public class Item {
+
     private int id;
+
     private String name;
+
+    private final LocalDateTime created = LocalDateTime.now();
 
     public Item() {
     }
@@ -30,5 +36,9 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
     }
 }
