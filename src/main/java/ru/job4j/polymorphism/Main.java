9da08fab51dca87fde75d187aeb365eb6138bee3
Viktor Ivannikov <iceivannikov@gmail.com>
2024-03-30 13:09:15 +0300

    4.0. Что такое Interface. [#504946]

---
+++ src/main/java/ru/job4j/polymorphism/Main.java
@@ -0,0 +1,17 @@
+package ru.job4j.polymorphism;
+
+public class Main {
+    public static void main(String[] args) {
+        Vehicle townCar = new TownCar();
+        townCar.changeGear();
+        townCar.accelerate();
+        townCar.steer();
+        townCar.brake();
+        Vehicle sportCar = new SportCar();
+        sportCar.changeGear();
+        sportCar.accelerate();
+        sportCar.steer();
+        sportCar.brake();
+        sportCar.refill();
+    }
+}
