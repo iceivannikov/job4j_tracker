8564409bf0d9d05acc2a88ec8b92a6b14b062408
Viktor Ivannikov <iceivannikov@gmail.com>
2024-04-02 20:14:18 +0300

    0.4. Пользовательские исключения. [#219367 #489263] исправлено

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -4,13 +4,13 @@ public class FindEl {
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
         int result = -1;
         for (int i = 0; i < value.length; i++) {
-            if (value[i] == null) {
-                throw new ElementNotFoundException("Element is null");
-            }
             if (key.equals(value[i])) {
                 result = i;
                 break;
             }
+        }
+        if (result == -1) {
+            throw new ElementNotFoundException("Element not found");
         }
         return result;
     }
