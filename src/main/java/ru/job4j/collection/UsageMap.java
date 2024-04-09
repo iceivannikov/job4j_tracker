aef500fd7fa639fc34a6df37623e87c5ef94d6df
Viktor Ivannikov <iceivannikov@gmail.com>
2024-04-09 18:27:08 +0300

    0. Map, HashMap [#212652 #490472] исправлено

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -9,6 +9,8 @@ public class UsageMap {
         Map<String, String> map = new HashMap<>();
         map.put("parsentev@yandex.ru", "Petr Arsentev");
         map.put("ivannikov@yandex.ru", "Viktor Ivannikov");
+        map.put("parsentev@yandex.ru", "Petr Arsentev 1");
+        map.put("ivannikov@yandex.ru", "Viktor Ivannikov 1");
         Set<Map.Entry<String, String>> entry = map.entrySet();
         for (Map.Entry<String, String> stringEntry : entry) {
             System.out.println(stringEntry.getKey() + "=" + stringEntry.getValue());
