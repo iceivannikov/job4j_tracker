57ccf7f50bccd9b3899407c17254270fa777a896
Viktor Ivannikov <iceivannikov@gmail.com>
2024-04-12 14:59:29 +0300

    2.0. Лямбда  [#58165]

--- src/main/java/ru/job4j/lambda/AttachmentSort.java
+++ src/main/java/ru/job4j/lambda/AttachmentSort.java
@@ -11,22 +11,14 @@ public class AttachmentSort {
                 new Attachment("image 2", 34),
                 new Attachment("image 3", 13)
         );
-        Comparator<Attachment> comparator = new Comparator<>() {
-            @Override
-            public int compare(Attachment o1, Attachment o2) {
-                return Integer.compare(o1.getSize(), o2.getSize());
-            }
-        };
-        attachments.sort(comparator);
+        Comparator<Attachment> comparatorText = (o1, o2) -> o1.getName().compareTo(o2.getName());
+        attachments.sort(comparatorText);
         System.out.println(attachments);
-        Comparator<Attachment> comparator2 = new Comparator<>() {
-
-            @Override
-            public int compare(Attachment o1, Attachment o2) {
-                return o1.getName().compareTo(o2.getName());
-            }
-        };
-        attachments.sort(comparator2);
+        Comparator<Attachment> comparatorDescSize = (o1, o2) -> Integer.compare(
+                o2.getName().length(),
+                o1.getName().length()
+        );
+        attachments.sort(comparatorDescSize);
         System.out.println(attachments);
     }
 }
