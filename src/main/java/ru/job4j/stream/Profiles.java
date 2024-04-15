cb0051a17bdfbf654ac330528586b970a586e031
Viktor Ivannikov <iceivannikov@gmail.com>
2024-04-15 15:57:51 +0300

    6. Список адресов [#110062 #491373]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -1,16 +1,12 @@
 package ru.job4j.stream;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class Profiles {
     public static List<Address> collect(List<Profile> profiles) {
         return profiles.stream()
-                .map(profile -> new Address(
-                        profile.address().getCity(),
-                        profile.address().getStreet(),
-                        profile.address().getHome(),
-                        profile.address().getApartment())
-                )
+                .map(Profile::address)
                 .toList();
     }
 }
