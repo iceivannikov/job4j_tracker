cdc56da122fbbf72312567680eab9c259c74bbf6
Viktor Ivannikov <iceivannikov@gmail.com>
2024-04-18 11:17:10 +0300

    0. Stream API улучшения [#58167 #491758] исправлено

--- src/main/java/ru/job4j/stream/StudentLevel.java
+++ src/main/java/ru/job4j/stream/StudentLevel.java
@@ -1,5 +1,6 @@
 package ru.job4j.stream;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -8,7 +9,7 @@ public class StudentLevel {
     public static List<Student> levelOf(List<Student> students, int bound) {
         return students.stream()
                 .filter(Objects::nonNull)
-                .sorted((left, right) -> right.getScore() - left.getScore())
+                .sorted(Comparator.comparingInt(Student::getScore))
                 .takeWhile(student -> student.getScore() >= bound)
                 .collect(Collectors.toList());
     }
