8b34ddb536a36f839f409247d1e911fe87446483
Viktor Ivannikov <iceivannikov@gmail.com>
2025-03-01 12:34:54 +0300

    239. Sliding Window Maximum

---
+++ src/main/java/ru/job4j/algorithms/SlidingWindowMaximum.java
@@ -0,0 +1,30 @@
+package ru.job4j.algorithms;
+
+import java.util.*;
+
+public class SlidingWindowMaximum {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int[] result = new int[nums.length - k + 1];
+        Deque<Integer> deque = new ArrayDeque<>();
+        for (int i = 0; i < nums.length; i++) {
+            while (!deque.isEmpty() && deque.peekFirst() < i - k + 1) {
+                deque.pollFirst();
+            }
+            while (!deque.isEmpty() && nums[deque.peekLast()] < nums[i]) {
+                deque.pollLast();
+            }
+            deque.offerLast(i);
+            if (!deque.isEmpty() && i >= k - 1) {
+                result[i - k + 1] = nums[deque.peekFirst()];
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 3, -1, -3, 5, 3, 6, 7};
+        int k = 3;
+        SlidingWindowMaximum swm = new SlidingWindowMaximum();
+        System.out.println(Arrays.toString(swm.maxSlidingWindow(nums, k)));
+    }
+}
