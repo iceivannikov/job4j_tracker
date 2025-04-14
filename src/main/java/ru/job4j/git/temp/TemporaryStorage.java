1a11237663d3a794b60eb59c07004ca45ebda8ce
Viktor Ivannikov <iceivannikov@gmail.com>
2025-04-14 22:15:25 +0300

    3.4. Временное хранение. git stash. [#504832]

---
+++ src/main/java/ru/job4j/git/temp/TemporaryStorage.java
@@ -0,0 +1,7 @@
+package ru.job4j.git.temp;
+
+public class TemporaryStorage {
+    public static void main(String[] args) {
+        System.out.println("3.4. Временное хранение. git stash. [#504832]");
+    }
+}
