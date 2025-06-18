4d7d1ffa2c28d98c3a02d43d5ff7f8b9b1eaf124
Viktor Ivannikov <iceivannikov@gmail.com>
2025-06-18 21:22:06 +0300

    66. Plus One

---
+++ src/main/java/ru/job4j/algorithms/PlusOne.java
@@ -0,0 +1,25 @@
+package ru.job4j.algorithms;
+
+import java.util.Arrays;
+
+public class PlusOne {
+    public int[] plusOne(int[] digits) {
+        int n = digits.length;
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            } else {
+                digits[i] = 0;
+            }
+        }
+        int[] result = new int[digits.length + 1];
+        result[0] = 1;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] digits = new int[]{1, 2, 3};
+        System.out.println(Arrays.toString(new PlusOne().plusOne(digits)));
+    }
+}
