68a361df3b0339840f8181c69558d37ec45de3df
Viktor Ivannikov <iceivannikov@gmail.com>
2025-08-06 17:09:08 +0300

    108. Convert Sorted Array to Binary Search Tree

---
+++ src/main/java/ru/job4j/algorithms/MaximumDepthOfBinaryTreeBFS.java
@@ -0,0 +1,49 @@
+package ru.job4j.algorithms;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class MaximumDepthOfBinaryTreeBFS {
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int level = 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            level++;
+        }
+        return level;
+    }
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
---
+++ src/main/java/ru/job4j/algorithms/MaximumDepthOfBinaryTreeDFS.java
@@ -0,0 +1,31 @@
+package ru.job4j.algorithms;
+
+public class MaximumDepthOfBinaryTreeDFS {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        return Math.max(left, right) + 1;
+    }
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
