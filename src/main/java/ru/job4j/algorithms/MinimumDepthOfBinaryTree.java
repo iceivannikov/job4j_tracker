6ce448b436f1f8ebf0db52d500362d0b4789eb78
Viktor Ivannikov <iceivannikov@gmail.com>
2025-08-20 17:37:57 +0300

    111. Minimum Depth of Binary Tree

---
+++ src/main/java/ru/job4j/algorithms/MinimumDepthOfBinaryTree.java
@@ -0,0 +1,54 @@
+package ru.job4j.algorithms;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class MinimumDepthOfBinaryTree {
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.left == null && root.right == null) {
+            return 1;
+        }
+        int level = 1;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode curNode = queue.poll();
+                if (curNode.left == null && curNode.right == null) {
+                    return level;
+                }
+                if (curNode.left != null) {
+                    queue.offer(curNode.left);
+                }
+                if (curNode.right != null) {
+                    queue.offer(curNode.right);
+                }
+            }
+            level++;
+        }
+        return level;
+    }
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
