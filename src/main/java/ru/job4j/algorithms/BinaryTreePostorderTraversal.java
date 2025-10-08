1d212ec95b450dc5add5227a17d636556d6fe87e
Viktor Ivannikov <iceivannikov@gmail.com>
2025-10-08 16:47:32 +0500

    145. Binary Tree Postorder Traversal

---
+++ src/main/java/ru/job4j/algorithms/BinaryTreePostorderTraversal.java
@@ -0,0 +1,63 @@
+package ru.job4j.algorithms;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class BinaryTreePostorderTraversal {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = root;
+        TreeNode prev = null;
+        while (!stack.isEmpty() || cur != null) {
+            if (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            } else {
+                TreeNode node = stack.peek();
+                if (node.right != null && node.right != prev) {
+                    cur = node.right;
+                } else {
+                    result.add(node.val);
+                    stack.pop();
+                    prev = node;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private void dfs(TreeNode root, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+        dfs(root.left, list);
+        dfs(root.right, list);
+        list.add(root.val);
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
+
