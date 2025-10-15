fecd7ae30eeb2bdc72f046e578a928bcfa34af02
Viktor Ivannikov <iceivannikov@gmail.com>
2025-10-15 15:37:22 +0500

    160. Intersection of Two Linked Lists

---
+++ src/main/java/ru/job4j/algorithms/IntersectionOfTwoLinkedLists.java
@@ -0,0 +1,23 @@
+package ru.job4j.algorithms;
+
+public class IntersectionOfTwoLinkedLists {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode pA = headA;
+        ListNode pB = headB;
+        while (pA != pB) {
+            pA = (pA == null) ? headB : pA.next;
+            pB = (pB == null) ? headA : pB.next;
+        }
+        return pA;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+}
