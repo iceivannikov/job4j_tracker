0101ad520b34b7a00b1084dbdef590d4ddca89b3
Viktor Ivannikov <iceivannikov@gmail.com>
2025-10-23 16:04:11 +0500

    168. Excel Sheet Column Title

---
+++ src/main/java/ru/job4j/algorithms/ExcelSheetColumnTitle.java
@@ -0,0 +1,14 @@
+package ru.job4j.algorithms;
+
+public class ExcelSheetColumnTitle {
+    public String convertToTitle(int columnNumber) {
+        StringBuilder sb = new StringBuilder();
+        while (columnNumber > 0) {
+            columnNumber--;
+            char ch = (char) ('A' + columnNumber % 26);
+            sb.append(ch);
+            columnNumber /= 26;
+        }
+        return sb.reverse().toString();
+    }
+}
